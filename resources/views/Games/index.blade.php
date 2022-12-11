8658a380c05c776f4d7ebe9bf3933e227e879b29
TaichiN27 <ragu.cap@icloud.com>
2022-12-11 14:59:08 +0900

    to parents page

--- resources/views/Games/index.blade.php
+++ resources/views/Games/index.blade.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
@@ -9,6 +10,7 @@
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     </head>
     <body>
+       <button id="parentsBtn" type="button" class="btn btn-light">保護者ページへ</button>
         <div class="container">
             <div class="animalImage text-center rounded shadow-lg bg-warning p-5">
                 <h1 class="pb-2">どーっちだ？？</h1>
